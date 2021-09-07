3eab831ced725374cc4534050a281a318e6c3897
Mark Williams <m.williams.3141@gmail.com>
2021-09-07 23:22:16 +0100

    Add default layout

---
+++ layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Content />
+    <slot/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Content from '@/components/Content'
+import Footer from '@/components/Footer'
+export default {
+  name: 'DefaultLayout',
+  components: { Footer, Content }
+}
+</script>
