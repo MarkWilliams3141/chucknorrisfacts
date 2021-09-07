6a96257bf5edf3a752c46efef59d51032acf9909
Mark Williams <m.williams.3141@gmail.com>
2021-09-07 23:17:52 +0100

    Add component placeholders

---
+++ src/components/Content.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <p>Content placeholder</p>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Content'
+})
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <p>Footer placeholder</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Footer'
+})
+</script>
