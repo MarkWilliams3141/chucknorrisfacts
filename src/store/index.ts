369f7bb58e7e9a8ae00eb35425532793e871943c
Mark Williams <m.williams.3141@gmail.com>
2021-09-07 23:35:33 +0100

    Setup vuex store to fetch 10 facts from remote API

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,32 @@
 import { createStore } from 'vuex'
 
+const API_URL = 'https://api.chucknorris.io/jokes'
+const API_CATEGORY = 'random'
+const INITIAL_FACTS = 10
+
 export default createStore({
   state: {
+    facts: [],
+    likes: [] // TODO: Add support for likes
   },
   mutations: {
+    SET_FACTS (state, facts) {
+      state.facts = facts
+    }
   },
   actions: {
+    async getFacts ({ commit }) {
+      //  https://api.chucknorris.io/ does not let us batch fetch facts so we need loop initial 10 facts
+      const requests = Array.apply(null, Array(INITIAL_FACTS)).map(() => fetch(`${API_URL}/${API_CATEGORY}`))
+
+      Promise.all(requests).then(function (responses) {
+        return Promise.all(responses.map((response) => response.json()))
+      }).then(function (data) {
+        commit('SET_FACTS', data)
+      }).catch(function (error) {
+        console.log(error) // Something went wrong
+      })
+    }
   },
   modules: {
   }
