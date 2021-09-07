7bdf612f93086e3dddc52ddd0b724148a018dea7
Mark Williams <m.williams.3141@gmail.com>
2021-09-07 23:47:35 +0100

    Change font to Consolas

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Consolas,monaco, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
