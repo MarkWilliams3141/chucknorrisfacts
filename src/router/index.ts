a1f6716cbc21cccd86adc18f8dafca1a26c652ad
Mark Williams <m.williams.3141@gmail.com>
2021-09-07 23:09:43 +0100

    Add router boilerplate with Index and Detail paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,22 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Index from '../views/Index.vue'
+import Detail from '../views/Detail.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: Index
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
