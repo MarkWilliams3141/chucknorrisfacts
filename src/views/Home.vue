4ef75d8ac57d6203cc438af673b8ea4ab98ed6de
Mark Williams <m.williams.3141@gmail.com>
2021-09-07 23:07:49 +0100

    Enforce linting rules across entire project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div v-if="true">
+      <div class="flex items-center justify-center p-5">
+        <img alt="Chuck Norris is so tough" src="../assets/chucknorris.png">
+      </div>
+      <ul id="facts">
+        <li></li>
+      </ul>
+    </div>
+    <div v-else class="flex flex-col items-center justify-center min-h-screen">
+      <img alt="Loading Chuck Norris facts" src="../assets/loading.gif">
+      <p>Loading...</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+  name: 'Index'
+})
 </script>
